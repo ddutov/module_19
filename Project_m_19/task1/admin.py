91ed77266502cfee765ceae4082cd645d540ac4e
ddutov <165052103+ddutov@users.noreply.github.com>
2024-09-27 10:55:21 +0300

    task1-4

--- Project_m_19/task1/admin.py
+++ Project_m_19/task1/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size')
+    # fields = [('title', 'cost', 'size'), 'description']
+    fieldsets = (
+        ('info', {
+            'fields':
+                ('title', 'cost', 'size')
+        }),
+        ('footer', {
+            'fields':
+                ('description',)
+        })
+    )
+    search_fields = ('title',)
+    list_filter = ('cost',)
+
+
+admin.site.register(Buyer)
+
